cfa3e57a1aab47bd10e101726dfb01f013a76d8d
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2022-08-22 13:31:17 +0600

    player name get

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// document.getElementById('all-player').addEventListener('click', function (element) {
+//     const select = element.parentNode()
+//     console.log(select);
+// })
+// // document.getElementsByTagName('h5')
+
+function addPlayer(element) {
+    console.log(element.parentNode.parentNode.children[1].children[0].innerText);
+
+}
